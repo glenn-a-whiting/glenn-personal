9070f06dce46f6072cce6d51c0e3c033a8f9c4fb
glenn.a.whiting <glenn.a.whiting@gmail.com>
2018-02-16 16:42:20 +1000

    removed phoneticAlphabet scope variable

--- angular/js/controllers.js
+++ angular/js/controllers.js
@@ -3,34 +3,7 @@ angular.module("app").controller("sampleCtrl", function($scope,str){
   $scope.palindrome = false;
   $scope.wordCount = 0;
   $scope.acronym = "";
-  $scope.phoneticAlphabet = {
-    "a":"Alpha",
-    "b":"Bravo",
-    "c":"Charlie",
-    "d":"Delta",
-    "e":"Echo",
-    "f":"Foxtrot",
-    "g":"Golf",
-    "h":"Hotel",
-    "i":"India",
-    "j":"Juliett",
-    "k":"Kilo",
-    "l":"Lima",
-    "m":"Mike",
-    "n":"November",
-    "o":"Oscar",
-    "p":"Papa",
-    "q":"Quebec",
-    "r":"Romeo",
-    "s":"Sierra",
-    "t":"Tango",
-    "u":"Uniform",
-    "v":"Victor",
-    "w":"Whiskey",
-    "x":"Xray",
-    "y":"Yankee",
-    "z":"Zulu"
-  };
+  
   $scope.assign = function(){
     $scope.var = str.reverse($scope.info);
     $scope.palindrome = str.isPalindrome($scope.info);

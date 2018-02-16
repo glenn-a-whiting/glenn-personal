42ad14d3fc84e6eb367b3cbfed3bdeb12ac678e3
glenn.a.whiting <glenn.a.whiting@gmail.com>
2018-02-16 23:07:03 +1000

    update to angular sample

--- angular/js/controllers.js
+++ angular/js/controllers.js
@@ -1,9 +1,15 @@
-angular.module("app").controller("sampleCtrl", function($scope,str){
+angular.module("app").controller("sampleCtrl", function($scope,$http,str){
   $scope.var = "";
   $scope.palindrome = false;
   $scope.wordCount = 0;
   $scope.acronym = "";
-  
+  $scope.dictionary = 0;
+  $http.get("C:/Users/Glenn/Documents/Resume/github/glenn-personal/angular/resources/dictionary.txt").then(function(response){
+    $scope.dictionary = Object.keys(response.data).length;
+  }).catch(function(err){
+    console.log(err);
+  });
+
   $scope.assign = function(){
     $scope.var = str.reverse($scope.info);
     $scope.palindrome = str.isPalindrome($scope.info);
